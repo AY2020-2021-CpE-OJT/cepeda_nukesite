54056cc5d8fdbf046e44366f61aa8dd52b4721a7
KaiEzeckai22 <kyle.z.xxii@gmail.com>
2021-06-18 06:27:50 +0800

    testing test.bat with basic_nukesite

---
+++ test.bat
@@ -0,0 +1,62 @@
+:START
+@echo off
+:START
+set /p command="Enter command: "
+IF '%command%' == 'exit' GOTO EXIT
+IF '%command%' == 'add' GOTO ADD
+IF '%command%' == 'commit' GOTO COMMIT_PROMPT
+IF '%command%' == 'full_reset' GOTO FULL_RESET
+IF '%command%' == 'log' GOTO LOG
+IF '%command%' == 'status' GOTO STATUS
+IF '%command%' == 'test' GOTO TEST
+IF '%command%' == 'push' GOTO PUSH_PROMPT
+IF '%command%' == 'cls' CLS
+GOTO START
+
+:ADD
+echo ">>>>>>>>>>>>>>>>> ADDING <<<<<<<<<<<<<<<<<<"
+git add .
+git status
+
+:COMMIT_PROMPT
+set /p commit_choice="Would you like to commit?: "
+IF '%commit_choice%' == 'yes' GOTO COMMIT_TRIGGER
+IF '%commit_choice%' == 'no' GOTO START
+GOTO COMMIT_PROMPT
+
+:COMMIT_TRIGGER
+echo ">>>>>>>>>>>>>>>>> COMMITING <<<<<<<<<<<<<<<<<<"
+set /p message="Enter message: "
+git commit -m "%message%"
+GOTO START
+
+:PUSH_PROMPT
+echo ">>>>>>>>>>>>>>>>> PUSHING <<<<<<<<<<<<<<<<<<"
+git status
+set /p push_choice="Confirm push?: "
+IF '%push_choice%' == 'yes' GOTO PUSH_TRIGGER
+IF '%push_choice%' == 'no' GOTO START
+GOTO PUSH_PROMPT
+
+:PUSH_TRIGGER
+git push
+GOTO START
+
+
+:TEST
+
+:FULL_RESET
+echo ">>>>>>>>>>>>>>>>> RESETTING <<<<<<<<<<<<<<<<<<"
+git reset
+GOTO START
+:LOG
+echo ">>>>>>>>>>>>>>>>> LOG <<<<<<<<<<<<<<<<<<"
+git log
+GOTO START
+
+:STATUS
+echo ">>>>>>>>>>>>>>>>> STATUS <<<<<<<<<<<<<<<<<<"
+git status
+GOTO START
+
+:EXIT
