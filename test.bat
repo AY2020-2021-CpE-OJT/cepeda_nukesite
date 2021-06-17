e303b17160dae8f6089582653afdc6d87343870d
KaiEzeckai22 <kyle.z.xxii@gmail.com>
2021-06-18 05:49:00 +0800

    update test.bat to contain add,reset,log,status,& push

--- test.bat
+++ test.bat
@@ -5,24 +5,52 @@
 :AFT _NIT
 set /p command="Enter command: "
 IF '%command%' == 'exit' GOTO EXIT
-IF '%command%' == 'tick' GOTO TICK
-IF '%command%' == 'add_commit' GOTO ADD_COMMIT
+IF '%command%' == 'add' GOTO ADD
+IF '%command%' == 'full_reset' GOTO FULL_RESET
+IF '%command%' == 'log' GOTO LOG
+IF '%command%' == 'status' GOTO STATUS
 IF '%command%' == 'test' GOTO TEST
+IF '%command%' == 'push' GOTO PUSH
+IF '%command%' == 'cls' CLS
 GOTO START
 
-:TICK
-set /p tick=%tick%+1
-echo %tick% 
-echo.
-pause >nul
+:ADD
+git add .
+git status
+echo adding
+
+:COMMIT_PROMPT
+set /p choice="Would you like to commit?: "
+IF '%choice%' == 'yes' GOTO COMMIT_TRIGGER
+IF '%choice%' == 'no' GOTO AFT _NIT
+GOTO COMMIT_PROMPT
+
+:COMMIT_TRIGGER
+set /p message="Enter message: "
+git commit -m %message%
+git log
 GOTO AFT _NIT
 
 
-:ADD_COMMIT
-git add .
-set /p message="Enter message: "
-git commit -m %message%
-
 :TEST
+
+:FULL_RESET
+git reset
+GOTO AFT _NIT
+:LOG
+git log
+GOTO AFT _NIT
+
+:STATUS
+git status
+GOTO AFT _NIT
+:PUSH
+git status
+:PUSH _PROMPT
+set /p choice="Confirm push?: "
+IF '%choice%' == 'yes' git push
+IF '%choice%' == 'no' GOTO AFT _NIT
+GOTO PUSH _PROMPT
+git push
 
 :EXIT
